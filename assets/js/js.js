82a486c0056e413b762748cfd6efacb17966c635
v.mayorova <root@WIN-OAS92DIMJE3.localdomain>
2025-10-15 22:34:35 +0500

    Правки

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,15 +1,29 @@
+function HideList(className, elems) {
+    // let description = document.getElementsByClassName(className)[0];
+    for (const listElement of elems) {
+        let id = listElement.id
+        let objList = document.getElementsByClassName(id)[0];
 
-
-const moreInfo= document.getElementsByClassName('dropdown')[0];
-
-moreInfo.addEventListener('click', e => {
-    let description = document.getElementById('description');
-    if(description.style.display === "none" ){
-        description.style.display = 'block'
-        moreInfo.style.fontWeight = 'lighter'
-
-    }else {
-        description.style.display = "none"
-        moreInfo.style.fontWeight = 'bold'
+        if (className === id) {
+            if (objList.classList.contains('hide-list')) {
+                objList.classList.remove('hide-list')
+                listElement.classList.add('select-button')
+            } else {
+                objList.classList.add('hide-list')
+                listElement.classList.remove('select-button')
+            }
+        } else if (!objList.classList.contains('hide-list')) {
+            objList.classList.add('hide-list')
+            listElement.classList.remove('select-button')
+        }
     }
-});
+}
+
+const lists = document.getElementsByClassName('dropdown')
+for (const list of lists) {
+    list.addEventListener('click', e => {
+        let currentList = list.id
+        HideList(currentList, lists)
+    });
+}
+
